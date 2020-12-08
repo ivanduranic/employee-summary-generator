244fa4b6a391a539608676f559a90853646bd39c
Ivan Duranic <ivan.duranic@gmail.com>
2020-12-07 20:01:12 -0500

    fixing bugs

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 const teamMembers = [];
-const idArray = [];
+const answerArray = [];
 
 function appSet() {
 
@@ -21,60 +21,27 @@ function appSet() {
       {
         type: "input",
         name: "managerName",
-        message: "Please enter your manager's name.",
-        validate: answer => {
-          if (answer !== "") {
-            return true;
-          }
-          return "Please enter at least one character.";
-        }
+        message: "Please enter your manager's name."
       },
       {
         type: "input",
         name: "managerId",
-        message: "Please enter your manager's id.",
-        validate: answer => {
-          const pass = answer.match(
-            /^[1-9]\d*$/
-          );
-          if (pass) {
-            return true;
-          }
-          return "Please enter a positive number greater than zero.";
-        }
+        message: "Please enter your manager's id."
       },
       {
         type: "input",
         name: "managerEmail",
-        message: "Please enter your manager's email.",
-        validate: answer => {
-          const pass = answer.match(
-            /\S+@\S+\.\S+/
-          );
-          if (pass) {
-            return true;
-          }
-          return "Please enter a valid email address.";
-        }
+        message: "Please enter your manager's email."
       },
       {
         type: "input",
         name: "managerOfficeNumber",
-        message: "Please enter your manager's office number.",
-        validate: answer => {
-          const pass = answer.match(
-            /^[1-9]\d*$/
-          );
-          if (pass) {
-            return true;
-          }
-          return "Please enter a positive number greater than zero.";
-        }
+        message: "Please enter your manager's office number."
       }
     ]).then(answers => {
       const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
       teamMembers.push(manager);
-      idArray.push(answers.managerId);
+      answerArray.push(answers.managerId);
       makeTeam();
     });
   }
@@ -111,62 +78,27 @@ function appSet() {
       {
         type: "input",
         name: "engineerName",
-        message: "Please enter your engineer's name.",
-        validate: answer => {
-          if (answer !== "") {
-            return true;
-          }
-          return "Please enter at least one character.";
-        }
+        message: "Please enter your engineer's name."
       },
       {
         type: "input",
         name: "engineerId",
-        message: "Please enter your engineer's id.",
-        validate: answer => {
-          const pass = answer.match(
-            /^[1-9]\d*$/
-          );
-          if (pass) {
-            if (idArray.includes(answer)) {
-              return "This ID is already taken. Please enter a different number.";
-            } else {
-              return true;
-            }
-                        
-          }
-          return "Please enter a positive number greater than zero.";
-        }
+        message: "Please enter your engineer's id."
       },
       {
         type: "input",
         name: "engineerEmail",
-        message: "Please enter your engineer's email.",
-        validate: answer => {
-          const pass = answer.match(
-            /\S+@\S+\.\S+/
-          );
-          if (pass) {
-            return true;
-          }
-          return "Please enter a valid email address.";
-        }
+        message: "Please enter your engineer's email."
       },
       {
         type: "input",
         name: "engineerGithub",
-        message: "Please enter your engineer's GitHub username.",
-        validate: answer => {
-          if (answer !== "") {
-            return true;
-          }
-          return "Please enter at least one character.";
-        }
+        message: "Please enter your engineer's GitHub username."
       }
     ]).then(answers => {
       const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
       teamMembers.push(engineer);
-      idArray.push(answers.engineerId);
+      answerArray.push(answers.engineerId);
       makeTeam();
     });
   }
@@ -176,62 +108,27 @@ function appSet() {
       {
         type: "input",
         name: "internName",
-        message: "Please enter your intern's name.",
-        validate: answer => {
-          if (answer !== "") {
-            return true;
-          }
-          return "Please enter at least one character.";
-        }
+        message: "Please enter your intern's name."
       },
       {
         type: "input",
         name: "internId",
-        message: "Please enter your intern's id.",
-        validate: answer => {
-          const pass = answer.match(
-            /^[1-9]\d*$/
-          );
-          if (pass) {
-            if (idArray.includes(answer)) {
-              return "This ID is already taken. Please enter a different number.";
-            } else {
-              return true;
-            }
-                        
-          }
-          return "Please enter a positive number greater than zero.";
-        }
+        message: "Please enter your intern's id."
       },
       {
         type: "input",
         name: "internEmail",
         message: "Please enter your intern's email.",
-        validate: answer => {
-          const pass = answer.match(
-            /\S+@\S+\.\S+/
-          );
-          if (pass) {
-            return true;
-          }
-          return "Please enter a valid email address.";
-        }
       },
       {
         type: "input",
         name: "internSchool",
-        message: "Please enter your intern's school.",
-        validate: answer => {
-          if (answer !== "") {
-            return true;
-          }
-          return "Please enter at least one character.";
-        }
+        message: "Please enter your intern's school."
       }
     ]).then(answers => {
       const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
       teamMembers.push(intern);
-      idArray.push(answers.internId);
+      answerArray.push(answers.internId);
       makeTeam();
     });
   }
